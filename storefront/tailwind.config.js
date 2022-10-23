7aa2aa95453565d17acc48da593cb88c3a1b9edb
Yinka Adedire <yinkakun@gmail.com>
2022-10-23 04:23:51 +0100

    setup font and colors config for tailwind

--- storefront/tailwind.config.js
+++ storefront/tailwind.config.js
@@ -9,6 +9,24 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: [`Kamerik`, ...defaultTheme.fontFamily.sans],
+        'alt-sans': [`Hamlin`, ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        noir: '#1a1a1a',
+        lotion: '#fafafa',
+        'monster-green': {
+          DEFAULT: '#95D600',
+          50: '#DDFF8F',
+          100: '#D7FF7A',
+          200: '#CAFF51',
+          300: '#BEFF29',
+          400: '#B1FF00',
+          500: '#95D600',
+          600: '#6E9E00',
+          700: '#476600',
+          800: '#202E00',
+          900: '#000000',
+        },
       },
     },
   },
